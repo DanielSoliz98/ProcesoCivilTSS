fa7f9e72f23f7704ddbaae07971904651c8ea1df
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-12-15 23:57:32 -0400

    Citacion implementada

--- src/Modelos/CitacionEmplazamiento.java
+++ src/Modelos/CitacionEmplazamiento.java
@@ -18,7 +18,17 @@ public class CitacionEmplazamiento extends Etapa {
     @Override
     public void ejecutarEtapa() {
         this.agregarMensaje("Inicia la etapa de Citacion y Emplazamiento");
-
+        if (DistribucionBernoulli.desicion(0.03)) {
+            this.agregarMensaje("Fallecimiento del demandado.");
+            this.agregarMensaje("Citacion a herederos bajo pena de nulidad.");
+            this.anadirDiasTranscurridos(TransformadaInversa.getX(5, 30));
+            this.finalizarProceso();
+        } else {
+            int contestacion = TransformadaInversa.getX(5, 30);
+            this.agregarMensaje("Contestacion a citacion por la parte demanda en " + contestacion + " dias.");
+            this.anadirDiasTranscurridos(contestacion);
+            this.finalizarEtapa();
+        }
         this.agregarMensaje("Termina la etapa de Citacion y Emplazamiento");
     }
 
