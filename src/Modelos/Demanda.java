5ae583fb0445837af0a9746981881a30d1382773
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-12-15 23:44:47 -0400

    Etapa de Demanda Implementada

--- src/Modelos/Demanda.java
+++ src/Modelos/Demanda.java
@@ -9,7 +9,7 @@ package Modelos;
  *
  * @author danie
  */
-public class Demanda extends Etapa{
+public class Demanda extends Etapa {
 
     public Demanda(Etapa etapa) {
         super("Demanda", etapa);
@@ -18,8 +18,41 @@ public class Demanda extends Etapa{
     @Override
     public void ejecutarEtapa() {
         this.agregarMensaje("Inicia la etapa de Demanda");
-        
+        this.revisionDemanda();
         this.agregarMensaje("Termina la etapa de Demanda");
     }
-    
+
+    private void revisionDemanda() {
+        int revision = TransformadaInversa.getX(1, 10);
+        this.agregarMensaje("Revision de demanda realizada en: " + revision + " dias.");
+        this.anadirDiasTranscurridos(revision);
+        if (DistribucionBernoulli.desicion(0.10)) {
+            this.agregarMensaje("Demanda defectuosa.");
+            this.subsanarDemanda();
+        } else {
+            this.citacionDemandado();
+        }
+    }
+
+    private void subsanarDemanda() {
+        double caso = NumerosAleatorios.getNumeroAleatorio();
+        if (caso >= 0 && caso <= 0.98) {
+            int subsanar = TransformadaInversa.getX(1, 3);
+            this.agregarMensaje("Subanar defectos de demanda en " + subsanar + " dias.");
+            this.anadirDiasTranscurridos(subsanar);
+            this.revisionDemanda();
+        } else if (caso > 0.98 && caso < 1) {
+            this.agregarMensaje("Demanda manifiestamente improponible");
+            this.anadirDiasTranscurridos(1);
+            this.finalizarProceso();
+        }
+    }
+
+    private void citacionDemandado() {
+        int citacion = TransformadaInversa.getX(1, 10);
+        this.agregarMensaje("Citacion a la parte demandada en " + citacion + " dias.");
+        this.anadirDiasTranscurridos(citacion);
+        this.finalizarEtapa();
+    }
+
 }
