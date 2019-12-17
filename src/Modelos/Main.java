f5ed9e71ebdb90f353aac29aadcb5c6a05b8c7b1
Daniel Soliz Vargar <danielsoliz98@hotmail.com>
2019-12-17 09:35:58 -0400

    Proceso civil terminado

--- src/Modelos/Main.java
+++ src/Modelos/Main.java
@@ -10,34 +10,34 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-//        ArrayList<ProcesoCivil> procesosSala1 = new ArrayList<>();
-//        procesosSala1.add(new ProcesoCivil(1));
-//        procesosSala1.add(new ProcesoCivil(2));
-//        procesosSala1.add(new ProcesoCivil(3));
-//        procesosSala1.add(new ProcesoCivil(4));
-//        procesosSala1.add(new ProcesoCivil(5));
+        ArrayList<ProcesoCivil> procesosSala1 = new ArrayList<>();
+        procesosSala1.add(new ProcesoCivil(1));
+        procesosSala1.add(new ProcesoCivil(2));
+        procesosSala1.add(new ProcesoCivil(3));
+        procesosSala1.add(new ProcesoCivil(4));
+        procesosSala1.add(new ProcesoCivil(5));
 //        
 //        ArrayList<ProcesoCivil> procesosSala2 = new ArrayList<>();
 //        procesosSala2.add(new ProcesoCivil(1));
 //        procesosSala2.add(new ProcesoCivil(2));
 //        procesosSala2.add(new ProcesoCivil(3));
 //        
-//        SalaCivil sala1 = new SalaCivil(1, procesosSala1);
+        SalaCivil sala1 = new SalaCivil(1, procesosSala1);
 //        SalaCivil sala2 = new SalaCivil(2, procesosSala2);
 //        SalaCivil sala3 = new SalaCivil(3, procesosSala2);
 //        SalaCivil sala4 = new SalaCivil(4, procesosSala1);
 //        SalaCivil sala5 = new SalaCivil(5, procesosSala2);
 //        
-//        sala1.start();
+        sala1.start();
 //        sala2.start();
 //        sala3.start();
 //        sala4.start();
 //        sala5.start();
-        ProcesoCivil cv = new ProcesoCivil(1);
-        ArrayList<String> mensajes = cv.getMensajesProceso();
-        for (String a : mensajes) {
-            System.out.println(a);
-        }
-        System.out.println(cv.getDiasTotalesTranscurridos());
+//        ProcesoCivil cv = new ProcesoCivil(1);
+//        ArrayList<String> mensajes = cv.getMensajesProceso();
+//        mensajes.forEach((a) -> {
+//            System.out.println(a);
+//        });
+//        System.out.println(cv.getDiasTotalesTranscurridos());
     }
 }
--- src/Modelos/SalaCivil.java
+++ src/Modelos/SalaCivil.java
@@ -18,21 +18,21 @@ public class SalaCivil extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Sala Civil " + this.idSala + ": Comienza a trabajar.");
+        System.out.println("Sala Civil " + this.getIdSala() + ": Comienza a trabajar.");
         for (int i = 0; i < procesos.size(); i++) {
             ProcesoCivil proceso = procesos.get(i);
-            System.out.println("Inicio del Proceso Civil: " + proceso.getIdProceso() + " SalaCivil: " + this.idSala);
-            ArrayList<Etapa> etapas = proceso.getEtapasProcesoCivil();
-            for (int j = 0; j < etapas.size(); j++) {
-                Etapa etapa = etapas.get(j);
-                System.out.println("Inicio Etapa: " + etapa.getNombreEtapa() + " Proceso Civil: " + proceso.getIdProceso());
-                etapa.ejecutarEtapa();
-                this.esperarXSegundos(2);
-                System.out.println("Fin Etapa: " + etapa.getNombreEtapa() + " Proceso Civil: " + proceso.getIdProceso());
-            }
-            System.out.println("Fin Proceso Civil: " + proceso.getIdProceso() + " SalaCivil: " + this.idSala);
+            System.out.println("Inicio del Proceso Civil: " + proceso.getIdProceso() + " SalaCivil: " + this.getIdSala());
+            proceso.getEtapasProcesoCivil().stream().map((e) -> {
+                e.getMensages().forEach((mensaje) -> {
+                    System.out.println("Proceso Civil " + proceso.getIdProceso() + ": " + mensaje);
+                });
+                return e;
+            }).forEachOrdered((e) -> {
+                this.esperarXSegundos(e.getDiasTranscurridosEtapa() / 10);
+            });
+            System.out.println("Fin Proceso Civil: " + proceso.getIdProceso() + " SalaCivil: " + this.getIdSala());
             System.out.println("Duracion de Proceso Civil " + proceso.getIdProceso() + ": "
-                    + proceso.getDiasTotalesTranscurridos() + " dias" + " SalaCivil: " + this.idSala);
+                    + proceso.getDiasTotalesTranscurridos() + " dias" + " SalaCivil: " + this.getIdSala());
         }
     }
 
@@ -42,5 +42,9 @@ public class SalaCivil extends Thread {
         } catch (InterruptedException ex) {
             SalaCivil.currentThread().interrupt();
         }
+    }
+
+    private int getIdSala() {
+        return this.idSala;
     }
 }
